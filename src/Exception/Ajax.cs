8097a681b50fd7bd4b73088138e92b383830a3e0
Fatih Aydin <faydinv1@gmail.com>
2014-01-13 10:22:13 +0000

    v1.0.3

---
+++ src/Exception/Ajax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace Utilities.Exception
+{
+    public static class Ajax
+    {
+        public static void ThrowException(HttpContext context, string message)
+        {
+            context.Response.ContentType = "text/HTML";
+            context.Response.Write("@ERROR:" + message);
+        }
+
+    }
+}
---
+++ src/Time/Epoch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Utilities.Time
+{
+    public class Epoch
+    {
+        public static string Get()
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var span = (DateTime.UtcNow - epoch);
+            return ((int)span.TotalSeconds / 7800).ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
